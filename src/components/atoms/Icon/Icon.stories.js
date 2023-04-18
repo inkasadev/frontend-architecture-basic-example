1bc408cae07c89952084e3e3aefd5eda161f0223
Phillipe Martins <hi@inkasa.dev>
2023-04-17 21:17:24 -0300

    chore: eslint npm script

--- src/components/atoms/Icon/Icon.stories.js
+++ src/components/atoms/Icon/Icon.stories.js
@@ -3,12 +3,12 @@ import React from "react";
 import { Icon } from "./Icon";
 
 export default {
-  title: "Atoms/Icon",
+	title: "Atoms/Icon",
 };
 
 const Template = (args) => <Icon {...args} />;
 
 export const Standard = Template.bind({});
 Standard.args = {
-  name: "fa-lemon",
+	name: "fa-lemon",
 };
--- src/components/atoms/Logo/Logo.stories.jsx
+++ src/components/atoms/Logo/Logo.stories.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Logo } from "./Logo";
 
 export default {
-  title: "Atoms/Logo",
+	title: "Atoms/Logo",
 };
 
 export const Standard = () => <Logo />;
